cb1c8fe942b2bc3316a31e888b2e3dcc1d6bc6db
HusYada <h_geyik@outlook.com>
2025-09-14 02:52:41 +0100

    hus stuff
    changes:
    + added title screen in Menu
    + added Pause Menu in the canvas, press P to pause, uses scripts SceneLoad & Pause
    + put in the updated ui stuff from the main branch
    + audio folder added
    - added simple footsteps check in PlayerMovement.cs, though the noise still plays when stunned
    - commented out gamemanager temporarily, feel free to uncomment and continue working on it

--- Assets/Script/GameManager/GameManager.cs
+++ Assets/Script/GameManager/GameManager.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Timer GameTimer;
+    // [SerializeField] private Timer GameTimer;
 
-    [Header("Setting")]
-    [SerializeField] private float gameTime;
+    // [Header("Setting")]
+    // [SerializeField] private float gameTime;
 
-    private void Start()
-    {
-        GameTimer.StartTimer(gameTime);
-    }
+    // private void Start()
+    // {
+    //     GameTimer.StartTimer(gameTime);
+    // }
 }
---
+++ Assets/Script/GameManager/Pause.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+                Resume();
+            else
+                PauseGame();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        AudioListener.volume = 0f;
+        isPaused = true;
+    }
+}
