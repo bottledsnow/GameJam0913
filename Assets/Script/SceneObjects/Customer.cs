8821ba359ec25c244a12b1193adf01d4e2b191a2
bottledsnow <snowmelt945gmail.com>
2025-09-14 14:21:55 +0800

    調整

--- Assets/Script/SceneObjects/Customer.cs
+++ Assets/Script/SceneObjects/Customer.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
-
+using System.Collections;
 public class Customer : MonoBehaviour
 {
     private Animator[] customers;
+    [SerializeField] private GameObject Parti_sing;
+    [SerializeField] private GameObject Parti_shock;
     private void Start()
     {
         customers = GetComponentsInChildren<Animator>();
+        Parti_sing.SetActive(true);
+        Parti_shock.SetActive(false);
     }
     public void Shock()
     {
@@ -13,12 +17,26 @@ public class Customer : MonoBehaviour
         {
             customer.SetBool("Shock",true);
         }
+        StartCoroutine(InShock());
     }
     public void resetShock()
     {
+        
         foreach (var customer in customers)
         {
+            customer.gameObject.SetActive(true);
             customer.SetBool("Shock", false);
         }
+        Parti_sing.SetActive(true);
+    }
+    IEnumerator InShock()
+    {
+        Parti_sing.SetActive(false);
+        yield return new WaitForSeconds(0.35f);
+        foreach (var customer in customers)
+        {
+            customer.gameObject.SetActive(false);
+        }
+        Parti_shock.SetActive(true);
     }
 }
